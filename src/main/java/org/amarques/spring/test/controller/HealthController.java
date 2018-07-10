c2c0a0d2213b27f0b8d106db221e54cada3216f7
alejandro-marques <amarques@stratio.com>
2018-07-10 09:50:29 +0200

    Changed port to 8080

--- src/main/java/org/amarques/spring/test/controller/HealthController.java
+++ src/main/java/org/amarques/spring/test/controller/HealthController.java
@@ -1,5 +1,7 @@
 package org.amarques.spring.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/health")
 public class HealthController {
+	private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
 	
 	@RequestMapping(value={"/ping"}, method=RequestMethod.GET, produces = "application/json")
 	public String ping () throws Exception{
+		logger.info("Received health request");
 		return "{\"status\": \"OK\"}";
 	}
 }
